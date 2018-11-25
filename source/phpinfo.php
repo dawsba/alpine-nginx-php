6ac2844a7eb99bfac4c3206a0724e2849e468829
Tobias Hargesheimer <tobiash87@gmail.com>
2018-11-25 21:17:58 +0100

    alpine+nginx+php
    first version

--- source/phpinfo.php
+++ source/phpinfo.php
@@ -1,10 +1,3 @@
 <?php
-
-// Zeigt alle Informationen (Standardwert ist INFO_ALL)
-phpinfo();
-
-// Zeigt nur die Modul-Informationen.
-// phpinfo(8) führt zum gleichen Ergebnis.
-#phpinfo(INFO_MODULES);
-
+	phpinfo();
 ?>
